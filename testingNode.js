eef6d2c0520a7d172deffa7c130ed9ebc3a582ea
Sarah Graupman <sarah.graupman@marquette.edu>
2018-02-12 17:26:24 -0600

    Add simpsons dataset

--- testingNode.js
+++ testingNode.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
-  fs.readFile('index.htm', function(err, data) {
+  fs.readFile('in-class.htm', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     res.end();
